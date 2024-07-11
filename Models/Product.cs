df5d44acda2a41ea36cd23c2f5354a629e8385f8
Samuel <padmadansamuel@gmail.com>
2024-07-12 00:19:52 +0530

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Models
+{
+    internal class Product
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private double _discount;
+
+        //Get Methods
+        public int GetId() { return _id; }
+        public string GetName() { return _name; }
+        public int GetPrice() { return _price; }
+        public double GetDiscount() { return _discount; }
+
+        //Set Methods
+        public void SetId(int id) { _id = id; }
+        public void SetName(string name) { _name = name; }
+        public void SetPrice(int price) { _price = price; }
+        public void SetDiscount(int discount) { _discount = discount; }
+        
+
+        public double CalculateDiscount()
+        {
+            double priceAfterDiscount = _price * (1 - (_discount / 100));
+            return priceAfterDiscount;
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.Models;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product productA = new Product();
+            Product productB = new Product();
+
+            //Entering product details
+
+            productA.SetId(1);
+            productA.SetName("product A");
+            productA.SetPrice(50);
+            productA.SetDiscount(21);
+
+
+            productB.SetId(2);
+            productB.SetName("product B");
+            productB.SetPrice(100);
+            productB.SetDiscount(10);
+
+            Console.WriteLine("Product A");
+            Console.WriteLine("Id : " + productA.GetId());
+            Console.WriteLine("Name : " + productA.GetName());
+            Console.WriteLine("Price : " + productA.GetPrice());
+            Console.WriteLine("Discount : " + productA.GetDiscount());
+            Console.WriteLine("Price after Discount is : " + productA.CalculateDiscount());
+            
+            Console.WriteLine("\nProduct B");
+            Console.WriteLine("Id : " + productB.GetId());
+            Console.WriteLine("Name : " + productB.GetName());
+            Console.WriteLine("Price : " + productB.GetPrice());
+            Console.WriteLine("Discount : " + productB.GetDiscount());
+            Console.WriteLine("Price after Discount is : " + productB.CalculateDiscount());
+
+
+
+
+        }
+    }
+}
